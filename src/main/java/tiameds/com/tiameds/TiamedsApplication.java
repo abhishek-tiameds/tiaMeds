d3eeafd17078ea8b0983f3dcc71469f46ae19fa9
abhishekritu01 <abhishekritu810219@gmail.com>
2024-11-30 13:04:25 +0530

    add jwt secreat value

--- src/main/java/tiameds/com/tiameds/TiamedsApplication.java
+++ src/main/java/tiameds/com/tiameds/TiamedsApplication.java
@@ -2,8 +2,10 @@ package tiameds.com.tiameds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yml")
 public class TiamedsApplication {
 
     public static void main(String[] args) {
--- src/main/java/tiameds/com/tiameds/utils/JwtUtil.java
+++ src/main/java/tiameds/com/tiameds/utils/JwtUtil.java
@@ -3,6 +3,7 @@ package tiameds.com.tiameds.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -13,7 +14,8 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    private String SECRET_KEY = "TaK+HaV^uvCHEFsEVfypW#7g9^k*Z8$V";
+    @Value("${spring.jwt.secret}")
+    private String SECRET_KEY;
 
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
